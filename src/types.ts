98010852b47aee033a7d01fd284fe431f5384321
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-06 11:42:04 +0400

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export interface User {
+    uid?:string,
+    type: string,
+    data:{
+        name:string,
+        password:string
+        error?:boolean,
+        errorText?:string
+    }
+    id:number
+}
+
+export enum PLAYER {
+    REG ='reg',
+    UPDATE_WINNERS='update_winners',
+}
+
+export enum ROOM {
+    CREATE_ROOM='create_room',
+    UPDATE_ROOM='update_room',
+    ADD_PLAYER='add_player_to_room',
+    START_GAME='start_game',
+    FINISH_GAME='finish'
+}
+
+export enum GAME {
+    ADD_SHIPS='add_ships',
+    ATTACK='attack',
+    RANDOM_ATTACK='randomAttack',
+    TURN='turn'
+}
+
+export interface RoomState {
+    type: string,
+    data:string,
+    id: number,
+}
