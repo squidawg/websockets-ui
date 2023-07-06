9596e82fcacbba2612ea081270a22e6401f6ef4d
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-06 11:43:19 +0400

    feat:  implement websocket server

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { httpServer } from "./http_server/index.js";
+import {RawData, WebSocketServer} from 'ws'
+import {commandController} from "./ws_server/commandController.js";
+import {ROOM} from "./types";
+const HTTP_PORT = 3000;
+
+const wsServer = new WebSocketServer({ server: httpServer });
+
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`Start static http server on the ${HTTP_PORT} port!`);
+});
+
+
+wsServer.on('connection', (connection, req) => {
+    console.log('received a new connection')
+    const id = String(req.headers['sec-websocket-key']);
+    connection.on('message', async (data:RawData) => {
+        const response = await commandController(id,data);
+        if(await response.type === ROOM.UPDATE_ROOM){
+            wsServer.clients.forEach(client =>{
+                client.send(JSON.stringify(response));
+            });
+        }
+        if(await response.type === 'add_user_to_room'){
+            wsServer.clients.forEach(client =>{
+                client.send(JSON.stringify(response));
+            });
+        }
+        else{
+            connection.send(JSON.stringify(response));
+        }
+    })
+})
