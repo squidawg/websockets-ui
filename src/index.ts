881f71ee9c3046ed833c4ca6dffc84ab4d4228e4
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-11 10:27:29 +0400

    fix: change logic for Ws responses

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,53 @@
 import { httpServer } from "./http_server/index.js";
 import WebSocket, {RawData, WebSocketServer} from 'ws'
 import {commandController} from "./ws_server/commandController.js";
-import {GAME, PLAYER, ROOM} from "./types.js";
+import {PLAYER, Response, ROOM} from "./types.js";
 const HTTP_PORT = 3000;
+
+interface CustomWebsocket extends WebSocket{
+    id?:number
+}
 
 httpServer.listen(HTTP_PORT, () => {
     console.log(`Start static http server on the ${HTTP_PORT} port!`);
 });
-
 const wsServer = new WebSocketServer({ server: httpServer });
-export let state = 0
 
-wsServer.on('connection', (connection, req) => {
-    console.log('received a new connection')
-
-    const id = String(req.headers['sec-websocket-key']);
-
+wsServer.on('connection', (connection:CustomWebsocket, req) => {
+    console.log('received a new connection');
+    const id = Math.floor(Math.random() * 10) + 1;
+    connection.id = id;
     connection.on('message', async (data:RawData) => {
-        const response =  commandController(id,data)!;
-        const cmd = response.at(0);
-        switch (cmd){
+        const response:Response = commandController(id,data)!;
+        switch (response.type){
             case PLAYER.REG:
-                connection.send(JSON.stringify(response.at(-1)));
+                connection.send(JSON.stringify(response.payload.at(0)));
                 break;
             case ROOM.UPDATE_ROOM:
-                wsServer.clients.forEach(client => {
-                    client.send(JSON.stringify(response.at(-1)));
+                wsServer.clients.forEach((client:CustomWebsocket) => {
+                    client.send(JSON.stringify(response.payload.at(0)));
                 });
                 break;
             case ROOM.ADD_PLAYER:
-                wsServer.clients.forEach(client =>{
-                    response.slice(1).forEach((res) => {
-                        client.send(JSON.stringify(res));
+                wsServer.clients.forEach((client:CustomWebsocket) => {
+                    response.payload.forEach((res) => {
+                        if(res.id === client.id){
+                            client.send(JSON.stringify(res));
+                        }
                     })
                 });
                 break;
             case ROOM.START_GAME:
-                if(wsServer.clients.size === 2){
-                    response.slice(1)!.forEach((res) => {
-                        connection.send(JSON.stringify(res));
-                        })
-                }
+                wsServer.clients.forEach((client:CustomWebsocket) => {
+                    response.payload.forEach((res) => {
+                        if(res.id === client.id){
+                            client.send(JSON.stringify(res));
+                        }
+                    })
+                });
                 break;
+            default:
+                break
         }
     })
 })
