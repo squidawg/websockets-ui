548f1972f336d967e738d4386f416c206d1b1102
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-11 10:26:01 +0400

    feat: add helper classes

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,59 @@ export interface Payload {
     id:number
 }
 
+export interface Ships {
+    positions:{}
+}
+export interface Response {
+    type:ROOM|PLAYER|string,
+    payload:Payload[]
+}
+
+export class User {
+    private readonly uid: number;
+    private readonly name: string;
+    private readonly password: string;
+    private ships:Ships[] = [];
+    constructor(uid:number,name:string, password: string) {
+        this.uid = uid;
+        this.name = name;
+        this.password = password;
+    }
+
+    setShips(ships:Ships[]){
+        this.ships = ships;
+    }
+    get getShips(){
+        return this.ships;
+    }
+    get getUserId(){
+        return this.uid;
+    }
+    get getName(){
+        return this.name;
+    }
+
+}
+
+export class Room {
+    private roomId:number;
+    private users: User[] = [];
+    constructor(roomId:number) {
+        this.roomId = roomId;
+    }
+    addUser(user:User){
+        this.users.push(user);
+    }
+    get getUsers(){
+        return this.users;
+    }
+
+    get getRoomId(){
+        return this.roomId;
+    }
+
+}
+
 export enum PLAYER {
     REG ='reg',
     UPDATE_WINNERS='update_winners',
